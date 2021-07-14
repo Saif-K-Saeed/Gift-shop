5feb044f7c249039151838f61e4f181d1fa64551
SAIFSAEED6 <>
2021-07-14 14:20:13 +0000

    js

---
+++ app.js
@@ -0,0 +1,25 @@
+var name = prompt("what is your name")
+
+alert("hey, wlecome in gift shop " + name )
+
+var type = prompt("input the type him , her or baby?")
+
+
+if(type == "him"){
+document.write(
+  "<div>" + "<h3>" + type+"</h3>"
++ "<img  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD_vvMxcFgjabeEL-KXLzSdCU5fLPdGdZ2wA&usqp=CAU' width='200' height='200'     />" + "</div>")
+}
+else if (type =="her"){
+document.write(
+  "<div>" + "<h3>" + type+"</h3>"
++ "<img  src='https://blog.grabon.in/wp-content/uploads/2017/01/Featured-image-25-Gift-ideas-for-her.jpg' width='100' height='100'     />" + "</div>")
+}
+else if(type =="baby"){
+document.write(
+  "<div>" + "<h3>" + type+"</h3>"
++ "<img  src='https://images.ctfassets.net/6m9bd13t776q/2NfQZpAV5p8fKPfAH2qQgi/0ce7322e46107dccacbe5feb942ecbaf/fauve-co-newborn-baby-gift-box-750x550.jpg?q=75' width='100' height='100'     />" + "</div>")
+}
+else{
+  alert('we dont have this type')
+}
